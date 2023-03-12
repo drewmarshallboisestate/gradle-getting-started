20e902b636fc606db157f8e15aaaee5335a17b12
Drew Marshall <drewmarshall@u.boisestate.edu>
2023-03-11 17:43:00 -0700

    Added my name

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -64,8 +64,9 @@ public class HerokuApplication {
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
         output.add("Read from DB: " + rs.getTimestamp("tick") + " " + rs.getString("random_string"));
-        // output.add("Read from DB: " + rs.getString("random_string"));
       }
+
+      System.out.println("Drew Marshall");
 
       model.put("records", output);
       return "db";
