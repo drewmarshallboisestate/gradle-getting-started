7762124c5ce6d5b23a7cf3de4cb4bb5e94f5070e
Drew Marshall <drewmarshall@u.boisestate.edu>
2023-03-11 17:35:33 -0700

    Fixed

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -63,8 +63,8 @@ public class HerokuApplication {
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-        output.add("Read from DB: " + rs.getString("random_string"));
+        output.add("Read from DB: " + rs.getTimestamp("tick") + " " + rs.getString("random_string"));
+        // output.add("Read from DB: " + rs.getString("random_string"));
       }
 
       model.put("records", output);
